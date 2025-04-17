b0a44699235db900280e02a316523e3e9ca47a45
Vemula1357 <vemula1357@gmail.com>
2025-04-16 22:32:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 st.title('My Streamlit App')
 
 # Reading the CSV file (replace with your actual CSV file path)
-df = pd.read_csv(''/Users/syduluvemula/Desktop/Foundation of Analytics/Exam Part 2/Automobiles.csv')
+df = pd.read_csv('/Users/syduluvemula/Desktop/Foundation of Analytics/Exam Part 2/Automobiles.csv'')
 
 # Display the DataFrame
 st.write(df)
