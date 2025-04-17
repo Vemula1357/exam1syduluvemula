bf206349900b19a52c0a9ab7a63fb7343a6bed3b
Vemula1357 <vemula1357@gmail.com>
2025-04-16 22:14:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 touch app.py
 
+import streamlit as st
+import pandas as pd
+
+# Title of the app
+st.title('My Streamlit App')
+
+# Reading the CSV file (replace with your actual CSV file path)
+df = pd.read_csv(''/Users/syduluvemula/Desktop/Foundation of Analytics/Exam Part 2/Automobiles.csv')
+
+# Display the DataFrame
+st.write(df)
 
